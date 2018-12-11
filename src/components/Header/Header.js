81590eaf7fb87799382bc27c34e295c77e30a42f
kspayne93 <kyle@investavante.com>
2018-12-10 17:00:15 -0700

    changed Class back to regular function

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,21 +2,12 @@ import React, {Component} from 'react';
 import './Header.css';
 
 
-class Header extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {};
-   }
-
-   render(){
+function Header () {
       return(
          <div id="header">
             <h1>Movie Tracker</h1>
          </div>
-      
-      
       )
-   }
-}
+};
 
 export default Header;
